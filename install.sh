d275bc627188da0bf9891a48fb6b47f0905ac779
Olivier <olivierstamour0952@gmail.com>
2016-06-20 20:27:58 -0400

    Updated install script

--- install.sh
+++ install.sh
@@ -5,4 +5,10 @@ DOTFILES_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 # vim
 ln -sfv "$DOTFILES_DIR/vim/vimrc" ~/.vimrc
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mkdir -p ~/.vim/autoload
+mkdir -p ~/.vim/bundles
+
 
